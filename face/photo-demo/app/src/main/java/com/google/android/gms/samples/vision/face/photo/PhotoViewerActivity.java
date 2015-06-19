1d3ef768ea4f146da62c956939efccf426049aae
Paul McCartney <mccartney@google.com>
2015-06-19 14:12:59 -0400

    added more comments for the isOperational case
    Change-Id: Id7a05beffebeba6d707a349862f0807768613cca

--- face/photo-demo/app/src/main/java/com/google/android/gms/samples/vision/face/photo/PhotoViewerActivity.java
+++ face/photo-demo/app/src/main/java/com/google/android/gms/samples/vision/face/photo/PhotoViewerActivity.java
@@ -19,6 +19,7 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseArray;
 
 import com.google.android.gms.vision.Frame;
@@ -32,6 +33,8 @@ import java.io.InputStream;
  * photo and displaying the photo with associated landmarks in the UI.
  */
 public class PhotoViewerActivity extends Activity {
+    private static final String TAG = "PhotoViewerActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +48,8 @@ public class PhotoViewerActivity extends Activity {
         // Setting "tracking enabled" to false is recommended for detection with unrelated
         // individual images (as opposed to video or a series of consecutively captured still
         // images).  For detection on unrelated individual images, this will give a more accurate
-        // result.
+        // result.  For detection on consecutive images (e.g., live video), tracking gives a more
+        // accurate (and faster) result.
         //
         // By default, landmark detection is not enabled since it increases detection time.  We
         // enable it here in order to visualize detected landmarks.
@@ -54,15 +58,21 @@ public class PhotoViewerActivity extends Activity {
                 .setLandmarkType(FaceDetector.ALL_LANDMARKS)
                 .build();
 
-        // Run face detection on the bitmap.
+        // Create a frame from the bitmap and run face detection on the frame.
         Frame frame = new Frame.Builder().setBitmap(bitmap).build();
         SparseArray<Face> faces = detector.detect(frame);
 
-        // Note: The first time that an app using face API is installed on a device, GMS will
-        // download a native library to the device in order to do detection.  Usually this
-        // completes before the app is run for the first time.  But if that download has not yet
-        // completed, then the above call will not detect any faces.  The detector.isOperational()
-        // method can be used to check if the required native library is currently available.
+        if (!detector.isOperational()) {
+            // Note: The first time that an app using face API is installed on a device, GMS will
+            // download a native library to the device in order to do detection.  Usually this
+            // completes before the app is run for the first time.  But if that download has not yet
+            // completed, then the above call will not detect any faces.
+            //
+            // isOperational() can be used to check if the required native library is currently
+            // available.  The detector will automatically become operational once the library
+            // download completes on device.
+            Log.w(TAG, "Face detector dependencies are not yet available.");
+        }
 
         FaceView overlay = (FaceView) findViewById(R.id.faceView);
         overlay.setContent(bitmap, faces);

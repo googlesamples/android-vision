e39a8e6b710be3184749c9d53165c21597b5147f
Paul Chang <paulchang@google.com>
2016-09-30 14:13:26 -0400

    Return tapped (or closest) barcode result to calling Activity.

--- visionSamples/barcode-reader/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ui/camera/GraphicOverlay.java
+++ visionSamples/barcode-reader/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ui/camera/GraphicOverlay.java
@@ -23,7 +23,9 @@ import android.view.View;
 import com.google.android.gms.vision.CameraSource;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.Vector;
 
 /**
  * A view which renders a series of custom graphics to be overlayed on top of an associated preview
@@ -51,7 +53,6 @@ public class GraphicOverlay<T extends GraphicOverlay.Graphic> extends View {
     private float mHeightScaleFactor = 1.0f;
     private int mFacing = CameraSource.CAMERA_FACING_BACK;
     private Set<T> mGraphics = new HashSet<>();
-    private T mFirstGraphic;
 
     /**
      * Base class for a custom graphics object to be rendered within the graphic overlay.  Subclass
@@ -129,7 +130,6 @@ public class GraphicOverlay<T extends GraphicOverlay.Graphic> extends View {
     public void clear() {
         synchronized (mLock) {
             mGraphics.clear();
-            mFirstGraphic = null;
         }
         postInvalidate();
     }
@@ -140,9 +140,6 @@ public class GraphicOverlay<T extends GraphicOverlay.Graphic> extends View {
     public void add(T graphic) {
         synchronized (mLock) {
             mGraphics.add(graphic);
-            if (mFirstGraphic == null) {
-                mFirstGraphic = graphic;
-            }
         }
         postInvalidate();
     }
@@ -153,22 +150,32 @@ public class GraphicOverlay<T extends GraphicOverlay.Graphic> extends View {
     public void remove(T graphic) {
         synchronized (mLock) {
             mGraphics.remove(graphic);
-            if (mFirstGraphic != null && mFirstGraphic.equals(graphic)) {
-                mFirstGraphic = null;
-            }
         }
         postInvalidate();
     }
 
     /**
-     * Returns the first (oldest) graphic added.  This is used
-     * to get the barcode that was detected first.
-     * @return graphic containing the barcode, or null if no barcodes are detected.
+     * Returns a copy (as a list) of the set of all active graphics.
+     * @return list of all active graphics.
      */
-    public T getFirstGraphic() {
+    public List<T> getGraphics() {
         synchronized (mLock) {
-            return mFirstGraphic;
+            return new Vector(mGraphics);
         }
+    }
+
+    /**
+     * Returns the horizontal scale factor.
+     */
+    public float getWidthScaleFactor() {
+        return mWidthScaleFactor;
+    }
+
+    /**
+     * Returns the vertical scale factor.
+     */
+    public float getHeightScaleFactor() {
+        return mHeightScaleFactor;
     }
 
     /**

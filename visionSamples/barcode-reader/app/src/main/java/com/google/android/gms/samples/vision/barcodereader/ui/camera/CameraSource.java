bb59dca961d2d6a5198aef5211c2f8a646b85891
IvanBean <ivan790530@gmail.com>
2017-10-02 14:17:22 +0800

    Make sure supportedPictureSizes order from large to small

--- visionSamples/barcode-reader/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ui/camera/CameraSource.java
+++ visionSamples/barcode-reader/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ui/camera/CameraSource.java
@@ -909,6 +909,12 @@ public class CameraSource {
                 parameters.getSupportedPreviewSizes();
         List<android.hardware.Camera.Size> supportedPictureSizes =
                 parameters.getSupportedPictureSizes();
+
+        int lastIndex = supportedPictureSizes.size() - 1;
+        if (supportedPictureSizes.get(0).height < supportedPictureSizes.get(lastIndex).height) {
+            Collections.reverse(supportedPictureSizes);
+        }
+
         List<SizePair> validPreviewSizes = new ArrayList<>();
         for (android.hardware.Camera.Size previewSize : supportedPreviewSizes) {
             float previewAspectRatio = (float) previewSize.width / (float) previewSize.height;

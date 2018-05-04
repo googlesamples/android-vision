86365a8f72b1b03805da73e56e4780e4d2126eb8
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2018-05-04 18:20:17 +0300

    add basic instrumentation test

---
+++ visionSamples/FaceTracker/app/src/androidTest/java/com/google/android/gms/samples/vision/face/facetracker/ExampleInstrumentedTest.java
@@ -0,0 +1,21 @@
+package com.google.android.gms.samples.vision.face.facetracker;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("ch.hepia.iti.opencvnativeandroidstudio", appContext.getPackageName());
+    }
+}

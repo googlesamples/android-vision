e10d6af12b1786b960ef53d4e783e73714ee3859
Ben Johns <baj@google.com>
2018-05-04 17:36:22 -0700

    Change to a better focus mode for modern devices.  Good luck, IO 2018ers.

--- visionSamples/ocr-codelab/ocr-reader-complete/app/src/main/java/com/google/android/gms/samples/vision/ocrreader/OcrCaptureActivity.java
+++ visionSamples/ocr-codelab/ocr-reader-complete/app/src/main/java/com/google/android/gms/samples/vision/ocrreader/OcrCaptureActivity.java
@@ -218,7 +218,7 @@ public final class OcrCaptureActivity extends AppCompatActivity {
                 .setRequestedPreviewSize(1280, 1024)
                 .setRequestedFps(2.0f)
                 .setFlashMode(useFlash ? Camera.Parameters.FLASH_MODE_TORCH : null)
-                .setFocusMode(autoFocus ? Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE : null)
+                .setFocusMode(autoFocus ? Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO : null)
                 .build();
     }
 

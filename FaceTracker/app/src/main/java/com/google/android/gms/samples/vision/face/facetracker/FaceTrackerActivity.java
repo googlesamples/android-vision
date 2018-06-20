32b5f9c24cc79e0e26263b4ad97569ea40bf7140
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2018-06-20 12:18:32 +0300

    disable opencv and cleanup

--- FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FaceTrackerActivity.java
+++ FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FaceTrackerActivity.java
@@ -49,6 +49,7 @@ import java.io.IOException;
 import java.util.List;
 
 import dlib.android.FaceRecognizer;
+import tensorflow.detector.spc.CameraActivityMain;
 
 /**
  * Activity for the face tracker app.  This app detects faces with the rear facing camera, and draws
@@ -235,7 +236,7 @@ public final class FaceTrackerActivity extends AppCompatActivity {
         mBtnDetect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(FaceTrackerActivity.this, OpenCvActivity.class);
+                Intent myIntent = new Intent(FaceTrackerActivity.this, CameraActivityMain.class);
                 FaceTrackerActivity.this.startActivity(myIntent);
             }
         });

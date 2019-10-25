82d3491fd342b2ff939c082c2b294a7cdc0af6c7
Randy <o1984531@gmail.com>
2019-10-25 16:32:31 +0800

    [Fix] Fix the clock not change when onResume

--- visionSamples/FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/flow/FaceSignInOutActivity.kt
+++ visionSamples/FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/flow/FaceSignInOutActivity.kt
@@ -18,10 +18,16 @@ class FaceSignInOutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_face_sign_in_out)
+    }
 
-        //customization
-        val calendar = Calendar.getInstance()
-        mBinding.cvacClock.setCalendar(calendar)
+    override fun onResume() {
+        super.onResume()
+
+        init()
+    }
+
+    fun init() {
+        mBinding.cvacClock.setCalendar(Calendar.getInstance())
             .setDiameterInDp(resources.getDimension(R.dimen.clock_face_diameter))
             .setOpacity(1.0f)
             .setShowSeconds(true)

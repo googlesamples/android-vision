73f2560cb71f9fcfde6acaed4c0b8bf66c1c87cb
Randy <o1984531@gmail.com>
2019-10-28 11:58:26 +0800

    [Add] Add related configs

--- visionSamples/FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/utils/Constants.kt
+++ visionSamples/FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/utils/Constants.kt
@@ -8,9 +8,23 @@ object Constants {
         const val SIGN_RESULT_PAGE_TIME_FORMAT = "HH:mm:ss"
         const val SIGN_RESULT_PAGE_DATE_FORMAT = "yyyy/MM/dd E"
     }
+
+    object Face {
+        const val EYE_OPEN_VALID_PROB = 0.3
+        const val FACE_VALID_CHECK_DURATION_SEC = 3L
+        const val TEMP_FACE_PHOTO_NAME = "temp_face.jpg"
         const val CAMERA_SOURCE_REQUEST_FPS = 30.0f
+        const val VALID_FACE_RETAIN_DURATION_MS = 1500L
     }
-    enum class SignType(val type:String) {
+
+    object Api {
+        val BASE_URL = " https://facelinkapi.azurewebsites.net"
+
+        val ERROR_TYPE_NO_FACE_DETECTED = 501
+        val ERROR_TYPE_MEMEMBER_Is_EXIST = 807
+    }
+
+    enum class SignType(val type: String) {
         SIGN_IN("SignIn"), SIGN_OUT("SignOut")
     }
 }

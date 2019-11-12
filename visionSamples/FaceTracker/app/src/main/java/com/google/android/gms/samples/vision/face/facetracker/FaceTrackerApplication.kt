8367a34388bd895336142038d534b6b707a43d40
Randy <o1984531@gmail.com>
2019-11-12 09:15:49 +0800

    [Add] Add Bugly tracking and in-app updating

--- visionSamples/FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FaceTrackerApplication.kt
+++ visionSamples/FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FaceTrackerApplication.kt
@@ -3,10 +3,12 @@ package com.google.android.gms.samples.vision.face.facetracker
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import com.google.android.gms.samples.vision.face.facetracker.utils.BuglyMgr
 
 class FaceTrackerApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+        BuglyMgr.buglyInit(this, BuildConfig.buglyID)
     }
 
     override fun attachBaseContext(base: Context?) {
